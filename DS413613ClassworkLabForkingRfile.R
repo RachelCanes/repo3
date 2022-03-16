02d426ec537931aa0920f036d69feeb09a11e144
Rachel Carnes <94029534+RachelCanes@users.noreply.github.com>
2022-03-16 18:04:35 -0400

    Update DS413613ClassworkLabForkingRfile.R
    The data source is the mpg data table

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Dennis P.
+# My name is Rachel Carnes.
 #The data source is the mpg data table
 # This is my coding that generates Boxplots for transmission types
+# Random line inserted here
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
